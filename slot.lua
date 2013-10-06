e8118419a0388c612e8f4f5b4b3a67f1b76577e6
tekkub :bear:  <tekkub@gmail.com>
2013-10-05 20:03:22 -0600

    Dead code

--- slot.lua
+++ slot.lua
@@ -7,9 +7,6 @@ function ns.MakeSlotFrame(bag, slot)
 	local frame = CreateFrame("Button", name, bag, "ContainerFrameItemButtonTemplate")
 	frame:SetID(slot)
 
-	frame:SetFrameLevel(bag:GetParent():GetFrameLevel()+10)
-	frame:SetFrameStrata(bag:GetParent():GetFrameStrata())
-
 	bag.slots[slot] = frame
 
 	return frame
