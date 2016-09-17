b190646a5f96191b8894eb1c7a4c39c2626fb22f
tekkub ʕ ´ᴥ` ʔ <git@tekkub.net>
2016-09-17 03:19:25 -0600

    nIlsafe

--- externals/item_colors.lua
+++ externals/item_colors.lua
@@ -4,7 +4,7 @@ local myname, ns = ...
 
 ns.item_colors = setmetatable({}, {
 	__index = function(t,i)
-		if i > 0 then
+		if i and i > 0 then
 			local r, g, b, hex = GetItemQualityColor(i)
 			local color = {r = r, g = g, b = b, hex = hex}
 			t[i] = color
