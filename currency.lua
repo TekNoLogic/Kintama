e4fbef7bad357a5a866d2e0f203df0a75673bb07
tekkub ʕ ´ᴥ` ʔ <git@tekkub.net>
2016-10-15 19:16:22 -0600

    Show currency limits

--- currency.lua
+++ currency.lua
@@ -31,8 +31,10 @@ end
 function ns.UpdateCurrency()
 	for i=1,MAX_WATCHED_TOKENS do
 		local frame = currencies[i]
-		local name, count, icon = GetBackpackCurrencyInfo(i)
+		local name, count, icon, currency_id = GetBackpackCurrencyInfo(i)
 		if name then
+			local _, _, _, _, _, max = GetCurrencyInfo(currency_id)
+			if max > 0 then count = count.."/"..max end
 			frame:SetText(count.. "|T".. icon.. ":12:12:0:0:64:64:4:60:4:60|t")
 			frame:Show()
 		else
