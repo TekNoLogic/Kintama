cef4073285d6b85f051befd54aea33609b7c2445
tekkub ʕ ´ᴥ` ʔ <git@tekkub.net>
2016-10-27 02:51:16 -0600

    Use new ilevel API

--- externals/ilvl.lua
+++ externals/ilvl.lua
@@ -2,20 +2,12 @@
 local myname, ns = ...
 
 
-assert(ns.scantip, "Tooltip scanner external not loaded")
-
-
-local re = _G.ITEM_LEVEL:gsub("%%d", "(%%d+)")
 ns.ilvls = setmetatable({}, {
 	__index = function(t,link)
-		ns.scantip:SetHyperlink(link)
-		for i=2,6 do
-			local text = ns.scantip.L[i]
-			local lvl = text and tonumber(text:match(re))
-			if lvl then
-				t[link] = lvl
-				return lvl
-			end
+		local ilvl = GetDetailedItemLevelInfo(link)
+		if ilvl then
+			t[link] = ilvl
+			return ilvl
 		end
 	end
 })
